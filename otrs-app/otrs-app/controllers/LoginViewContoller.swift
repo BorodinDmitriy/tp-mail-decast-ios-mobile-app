52c7e3bc5944cfd7bd470d41bfd48dad3b2b3e23
HustonMmmavr <huston.mavr@gmail.com>
2018-11-27 15:33:40 +0300

    added login controller

---
+++ otrs-app/otrs-app/controllers/LoginViewContoller.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewContoller.swift
+//  otrs-app
+//
+//  Created by Imber Fox on 27/11/2018.
+//  Copyright © 2018 Imber Fox. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewContoller: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var logoImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginAction(_ sender: Any) {
+        
+        // check which button clicked
+        // login guard get data from fields
+        // send seg or push controller
+        // also if register another seg or pushcontroller
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
