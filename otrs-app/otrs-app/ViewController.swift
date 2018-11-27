b757b10afedd9fd95750269d309f843d01408a39
HustonMmmavr <huston.mavr@gmail.com>
2018-11-27 13:57:23 +0300

    started new project

---
+++ otrs-app/otrs-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  otrs-app
+//
+//  Created by Imber Fox on 27/11/2018.
+//  Copyright © 2018 Imber Fox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ otrs-app/otrs-appTests/otrs_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  otrs_appTests.swift
+//  otrs-appTests
+//
+//  Created by Imber Fox on 27/11/2018.
+//  Copyright © 2018 Imber Fox. All rights reserved.
+//
+
+import XCTest
+@testable import otrs_app
+
+class otrs_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
